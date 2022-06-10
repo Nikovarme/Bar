3815fd7463c1975730034cfd6617c9ac5bfae842
NikoVarMe <omiadzenikusha8@gmail.com>
2022-06-10 16:48:36 +0400

    Niko's Bar

---
+++ styles.css/java/scripts.js
@@ -0,0 +1,5 @@
+function showproduct(){
+  //  document.getElementById("innernav").style.display= "block" ;
+  let display=document.getElementById("innernav").style.display;
+  console.log(display);
+}
